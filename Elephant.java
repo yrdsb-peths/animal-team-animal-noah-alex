266ed12bc4b8ba8782390f1e1cd48fdafffe753d
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-05-12 11:37:16 -0400

    added elephant

---
+++ Elephant.java
@@ -0,0 +1,93 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Elephant extends Actor
+{
+    GreenfootSound elephantSound = new GreenfootSound("comical elephant fart.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[10];
+    GreenfootImage[] idleLeft = new GreenfootImage[10];
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    public Elephant()
+    {
+        for (int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant-idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        animationTimer.mark();
+        
+        setImage(idleRight[0]);
+    }
+    
+    {
+        for (int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant-idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        setImage(idleRight[0]);
+    }
+    
+    
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left"))
+        {
+            move(-4);
+            facing = "left";
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(4);
+            facing = "right";
+        }
+        
+        eat();
+        
+        animateElephant();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.spawnApple();
+            world.increaseScore();
+            elephantSound.play();
+        }
+    }
+}
