4560c44b21d545de7dcfbec1d51c7dc0d4119c50
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-05-12 11:55:46 -0400

    fixed myworld

--- MyWorld.java
+++ MyWorld.java
@@ -1,46 +1,43 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
+    
     public MyWorld() {
         super(600, 400, 1, false);
-        //creates new elephant
-        Elephant ele = new Elephant();
-        addObject(ele, 300, 300);
         
-        //creates a label for score
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
+        
         scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
         
-        //references the apple method 
-        createApple();
+        spawnApple();
     }
     
     public void gameOver()
     {
-        //creates a new label after you die, which is game over
         Label gameOverLabel = new Label("Game Over", 100);
         addObject(gameOverLabel, 300, 200);
     }
     
     public void increaseScore()
     {
-        //increases score and changes the label to match
         score++;
         scoreLabel.setValue(score);
         
-        if(score % 5 ==0)
+        if(score % 5 == 0)
         {
-            level += 1;
+            level+= 1;
         }
     }
     
-    public void createApple()
-    {   
-        //creates a new apple object
+    public void spawnApple()
+    {
         Apple apple = new Apple();
-        //changes the speed of the apple falling every 5 seconds
         apple.setSpeed(level);
-        //falls randomly and sets the place it falls
         int x = Greenfoot.getRandomNumber(600);
         int y = 0;
         addObject(apple, x, y);
